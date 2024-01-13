c75fa28ac98378bea0874f0d303ab740921be136
Digvijay <digvijay.kewale@yahoo.co.in>
2024-01-13 22:11:09 +0530

    first commit

---
+++ text_editor.py
@@ -0,0 +1,56 @@
+import tkinter as tk
+from tkinter.filedialog import askopenfilename, asksaveasfilename 
+
+def open_file(window, text_edit):
+    filepath= askopenfilename(filetypes=[("Text Files", "*.txt*")])
+
+    if not filepath:
+        return
+
+    text_edit.delete(1.0,tk.END)
+    with open(filepath,"r") as f:
+        content = f.read()
+        text_edit.insert(tk.END, content)
+    window.title(f"openfile:{filepath}")
+
+
+def save_file(window, text_edit):
+    filepath = asksaveasfilename(filepath=[("Text Files", "*.txt*")])
+
+    if not filepath:
+        return
+    
+    with open(filepath, "w") as f:
+        content = text_edit.get(1.0, tk=END)
+        f.write(content)
+    f.write.title(f"open fiel:{filepath}")
+
+
+
+def main():
+    window = tk.Tk()
+    window.title("Text Editor")
+
+    window.rowconfigure(0,minsize=400)
+    window.columnconfigure(1,minsize=500)
+
+    text_edit = tk.Text(window, font="Helvetica 18")
+    text_edit.grid(row=0, column=1)
+
+    frame= tk.Frame(window,relief=tk.RAISED, bd=2)
+
+
+    save_button = tk.Button(frame, text=("save"), command= lambda:save_file(window, text_edit))
+    open_button = tk.Button(frame, text=("Open"), command= lambda:open_file(window, text_edit))
+
+    save_button.grid(row=0, column=0, padx=5, pady =5, sticky="ew")
+    open_button.grid(row=1, column=0, pady =5, padx=5, sticky="ew")
+    frame.grid(row=0, column=0, sticky="ns")
+
+
+    window.bind("<Control-s>", lambda a :save_file(window, text_edit))
+    window.bind("<Control-o>", lambda b :open_file(window, text_edit))
+    
+    window.mainloop()
+
+main()
